1d8e10eaeb92a1877dbd88826a0fb47ccfbe35eb
NguyenTanDat <nguyendat11577@gmail.com>
2024-03-26 21:39:50 +0700

    feat(Category): filter product

---
+++ frontend/src/component/Category/Category.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Category = ({ filterProducts }) => {
+    return (
+        <div>
+            <div className="electronics mb-40">
+                <h3 className="sidebar-title">Category</h3>
+                <div id="shop-cate-toggle" className="category-menu sidebar-menu sidbar-style">
+                    <ul>
+                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts(null)}>ALL</button></li>
+                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('LAPTOP')}>LAPTOP</button></li>
+                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('RAM')}>RAM</button></li>
+                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('MOUSE')}>MOUSE</button></li>
+                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('KEYBOARD')}>KEYBOARD</button></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Category;
