85ff57a726a71a04fcf62674b2b939b99baf5404
NguyenTanDat <nguyendat11577@gmail.com>
2024-03-28 16:18:28 +0700

    fix[Category, ProductItem]: fix err not find list product by type

--- frontend/src/component/Category/Category.js
+++ frontend/src/component/Category/Category.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Category = ({ filterProducts }) => {
+
+    const category =({ty})=>{
+        console.log(ty)
+    }
+
     return (
         <div>
             <div className="electronics mb-40">
                 <h3 className="sidebar-title">Category</h3>
                 <div id="shop-cate-toggle" className="category-menu sidebar-menu sidbar-style">
                     <ul>
-                        <li className="has-sub"><button className="nav-link" onClick={() => filterProducts(null)}>ALL</button></li>
+                        <li className="has-sub"><button className="nav-link" onClick={category("all")}>ALL</button></li>
                         <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('LAPTOP')}>LAPTOP</button></li>
                         <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('RAM')}>RAM</button></li>
                         <li className="has-sub"><button className="nav-link" onClick={() => filterProducts('MOUSE')}>MOUSE</button></li>
